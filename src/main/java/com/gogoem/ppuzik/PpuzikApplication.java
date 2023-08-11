9756628301e288c28eee7e50340bd807276f8456
goemgo <ekf0412@naver.com>
2023-08-11 18:30:16 +0900

    first commit

---
+++ src/main/java/com/gogoem/ppuzik/PpuzikApplication.java
@@ -0,0 +1,13 @@
+package com.gogoem.ppuzik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpuzikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpuzikApplication.class, args);
+	}
+
+}
